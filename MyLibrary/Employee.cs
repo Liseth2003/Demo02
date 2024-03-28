a05eabad6ebae4121f95d5dc3c487feaa2237cf4
Dianlly Colorado <msdsc4@gmailcom>
2024-03-28 12:45:59 -0500

    Clase Employee

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CalculateMonthlyPayment() 
+        {
+            
+        }
+    }
+
+    public class Secretary : Employee 
+    {
+        
+    }
+
+    public class Manager : Employee
+    {
+
+    }
+}
